b4fcf6184ff34ae427cc0a8188cac30e7f3414c4
Pondpdpr <pBoonnud@gmail.com>
2024-05-15 00:58:56 +0700

    connect history page to BE

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,19 +1,17 @@
+import getLog from "@/api/getLog";
+import { useEffect, useState } from "react";
+
 export default function History() {
-  // const [data, setData] = useState([]);
-  const data = [
-    {
-      dateTime: "12/09/2024 15:00:00",
-      username: "Sarah John",
-      concertName: "The festival Int 2024",
-      action: "Cancel",
-    },
-    {
-      dateTime: "12/09/2024 15:00:00",
-      username: "Sarah John",
-      concertName: "The festival Int 2024",
-      action: "Reserve",
-    },
-  ];
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const payload = await getLog();
+      setData(payload);
+    };
+    fetchData();
+  }, []);
+
   return (
     <main className={`flex flex-col h-full min-h-screen items-center py-16 px-10 bg-primaryBG`}>
       <div className="grid grid-cols-4 w-full border-[1px] border-[#5B5B5B] rounded-[4px]">
@@ -24,13 +22,13 @@ export default function History() {
         {data.map((action) => (
           <>
             <div className="border-r-[1px] border-t-[1px] border-[#5B5B5B] py-[10px] px-[12px] text-[16px] font-[400]">
-              {action.dateTime}
+              {action.created_at}
             </div>
             <div className="border-r-[1px] border-t-[1px] border-[#5B5B5B] py-[10px] px-[12px] text-[16px] font-[400]">
               {action.username}
             </div>
             <div className="border-r-[1px] border-t-[1px] border-[#5B5B5B] py-[10px] px-[12px] text-[16px] font-[400]">
-              {action.concertName}
+              {action.concertname}
             </div>
             <div className="border-t-[1px] border-[#5B5B5B] py-[10px] px-[12px] text-[16px] font-[400]">
               {action.action}

3112eb34602921e553065e20a2467bd581a02950
Pondpdpr <pBoonnud@gmail.com>
2024-05-18 19:59:58 +0700

    fix REST method of cancellation

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -59,7 +59,7 @@ export default function UserPage() {
 
   const cancel = async (reservationId: string) => {
     const response = await fetch(`${BACKEND_URL}/reservation/cancel/${reservationId}`, {
-      method: "POST",
+      method: "PUT",
       headers: {
         "Content-Type": "application/json",
         Authorization: "Bearer " + session?.access_token,
